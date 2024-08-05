6b07254f98f4e19e1adbea846908855c124224ef
HyonWooKang <87benzenes@gmail.com>
2024-08-05 18:35:11 +0900

    홈 화면 제작

---
+++ src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Home = () => {
+  const navigate = useNavigate();
+
+  const handlePageMove = (url) => {
+    return (event) => {
+      event.preventDefault();
+      navigate(url);
+    };
+  };
+
+  return (
+    <>
+      <head>This is a React Hook study page by Spencer</head>
+      <main>
+        <div style={{ margin: "20px" }}>
+          <h3>목록</h3>
+          <ul>
+            <li style={{ margin: "10px" }}>
+              <a href="" onClick={handlePageMove("/state")}>
+                useState로 이동
+              </a>
+            </li>
+            <li style={{ margin: "10px" }}>
+              <a href=""></a>
+            </li>
+            <li style={{ margin: "10px" }}>
+              <a href=""></a>
+            </li>
+          </ul>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/pages/UseContext.jsx
@@ -0,0 +1,3 @@
+const UseContext = () => {};
+
+export default UseContext;
